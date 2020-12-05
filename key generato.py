47b9936f3be768a3bc143f668100c76b83c338de
Ibragim <45774457+arbahaam@users.noreply.github.com>
2020-12-05 20:40:45 +0000

    Add files via upload

---
+++ key generato.py
@@ -0,0 +1,12 @@
+from Crypto.PublicKey import RSA
+
+key = RSA.generate(2048)
+private_key = key.exportKey()
+file_out=open("private.pem","wb")
+file_out.write(private_key)
+print(private_key)
+
+public_key = key.publickey().exportKey()
+file_out = open("receiver.pem","wb")
+file_out.write(public_key)
+print(public_key)
